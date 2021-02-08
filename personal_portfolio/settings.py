3105106211229e77cc62fc7616052f4cba42dedd
Christomesh <meshacholuwadamilare@gmail.com>
2021-02-08 01:09:41 +0100

    new local settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -125,3 +125,8 @@ STATIC_ROOT =  BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT =  BASE_DIR / 'media'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local fine. You must be on production")
